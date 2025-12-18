a9be5a75cb118092fcd82903cb7e6d61aa45374a
GodwinAkhidue <ebehiakhidue2099@gmail.com>
2025-12-18 09:51:20 +0100

    Update api.tsx

--- utils/api.tsx
+++ utils/api.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const server_url = "http://localhost:5000";
-// const server_url = "https://api.proclassics.co";
+const server_url = "https://api.proclassics.co";
 
 const api = axios.create({
   baseURL: server_url,
