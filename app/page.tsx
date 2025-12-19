cd5817baf9701acf5d346c2182c0017910a8bd6f
GodwinAkhidue <ebehiakhidue2099@gmail.com>
2025-12-19 13:21:53 +0100

    saf

--- app/page.tsx
+++ app/page.tsx
@@ -202,7 +202,7 @@ export default function Home() {
               className="w-full h-full object-cover"
               fill
               alt="collection pic"
-              src={"/collection1.jpg"}
+              src={"/collection1.JPG"}
             />
           </div>
           <div className="w-full h-full relative rounded-lg overflow-hidden">
@@ -218,7 +218,7 @@ export default function Home() {
               className="w-full h-full object-cover"
               fill
               alt="collection pic"
-              src={"/collection3.png"}
+              src={"/collection3.PNG"}
             />
           </div>
           <div className="w-full h-full relative rounded-lg overflow-hidden">
